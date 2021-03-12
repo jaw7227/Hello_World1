7f7c633cf6042a344347940d78319dc6ead70fc8
Jaw_7227 <akg7227@gmail.com>
2021-03-12 22:08:42 +0530

    cin >> n

--- main.cpp
+++ main.cpp
@@ -2,5 +2,10 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int n;
+    cin >> n;
+    for(auto i = 0;  i< n; i++)
+        cout << x << " ";
+    cout << endl;
     return 0;
 }
